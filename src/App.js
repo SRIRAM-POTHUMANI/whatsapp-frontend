35ae4b650d34f641920ef5f3afef36093a81ff9d
SRIRAM-POTHUMANI <84328532+SRIRAM-POTHUMANI@users.noreply.github.com>
2021-11-26 05:23:40 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -10,28 +10,52 @@ function App() {
   useEffect(() => {
     setUsername(prompt('Please enter your name'));
   }, []) 
-
+  const pusher = new Pusher('fbea830eb5fa8b8ee57d', {
+    cluster: 'ap2'
+  }); 
   const [messages, setMessages]= useState([]); 
-  useEffect(()=>{
-      axios.get('/messages/sync').then((response) =>{
-        setMessages(response.data)
-      })
-  },[])
+
+  const  sync = async ()=> {
+    await axios.get('/messages/sync')
+    .then((res)=>{
+      console.log(res.data);
+      setMessages(res.data)
+    })
+  }
   useEffect(() => {
-    var pusher = new Pusher('fbea830eb5fa8b8ee57d', {
-      cluster: 'ap2'
-    }); 
-console.log(messages)
-    var channel = pusher.subscribe('messages');
-    channel.bind('inserted', (newMessage) => {
-      // alert(JSON.stringify(newMessage));
-      setMessages([...messages,newMessage])
+   sync()
+  },  [])
+  
+
+  useEffect(() => {
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', function(newMessage) {
+      sync()
     });
-   return ()=>{
-      channel.unbind_all();
-      channel.unsubscribe();
-    };
-  },[messages]);
+    return ()=>{
+          channel.unbind_all();
+          channel.unsubscribe();
+        };
+   },  [messages])
+  
+  // useEffect(()=>{
+  //     axios.get('/messages/sync').then((response) =>{
+  //       setMessages(response.data)
+  //     })
+  // },[])
+  // useEffect(() => {
+    
+  // console.log(messages)
+  //   var channel = pusher.subscribe('messages');
+  //   channel.bind('inserted', (newMessage) => {
+  //     // alert(JSON.stringify(newMessage));
+  //     setMessages([...messages,newMessage])
+  //   });
+  //  return ()=>{
+  //     channel.unbind_all();
+  //     channel.unsubscribe();
+  //   };
+  // },[messages]);
   return (
     <div className="app">
       <div className="app_body">
